c98aa7514fa505e97d86a42be266e2b019fcf692
Leo <leoshch@gmail.com>
2018-12-13 16:09:04 +0000

    Update main-test.sh

--- main-test.sh
+++ main-test.sh
@@ -3,17 +3,18 @@ n=1
 iodepth=1
 #block=4
 #jobs=1
-#times=180
+times=600
+
 while [ $iodepth -le 4 ]
 do
-    ./fio_single_test.sh aws_multi_luns $n randwrite 0 4K $iodepth 180 1
-    ./fio_single_test.sh aws_multi_luns $n randread 0 4K $iodepth 180 1
-    ./fio_single_test.sh aws_multi_luns $n randwrite 0 8K $iodepth 180 1
-    ./fio_single_test.sh aws_multi_luns $n randread 0 8K $iodepth 180 1
-    ./fio_single_test.sh aws_multi_luns $n randwrite 0 16K $iodepth 180 1
-    ./fio_single_test.sh aws_multi_luns $n randread 0 16K $iodepth 180 1 
-    ./fio_single_test.sh aws_multi_luns $n randwrite 0 32K $iodepth 180 1
-    ./fio_single_test.sh aws_multi_luns $n randread 0 32K $iodepth 180 1
+    ./fio_single_test.sh aws_multi_luns $n randwrite 0 4K $iodepth $times 1
+    ./fio_single_test.sh aws_multi_luns $n randread 0 4K $iodepth $times 1
+    ./fio_single_test.sh aws_multi_luns $n randwrite 0 8K $iodepth $times 1
+    ./fio_single_test.sh aws_multi_luns $n randread 0 8K $iodepth $times 1
+    ./fio_single_test.sh aws_multi_luns $n randwrite 0 16K $iodepth $times 1
+    ./fio_single_test.sh aws_multi_luns $n randread 0 16K $iodepth $times 1 
+    ./fio_single_test.sh aws_multi_luns $n randwrite 0 32K $iodepth $times 1
+    ./fio_single_test.sh aws_multi_luns $n randread 0 32K $iodepth $times 1
     let n++ 
     let iodepth++
 done
@@ -22,12 +23,12 @@ n=10
 iodepth=1
 while [ $iodepth -le 5 ]
 do
-    ./fio_single_test.sh aws_multi_luns $n write 0 64K $iodepth 180 1
-    ./fio_single_test.sh aws_multi_luns $n read 0 64K $iodepth 180 1
-    ./fio_single_test.sh aws_multi_luns $n write 0 128K $iodepth 180 1
-    ./fio_single_test.sh aws_multi_luns $n read 0 128K $iodepth 180 1
-    ./fio_single_test.sh aws_multi_luns $n write 0 256K $iodepth 180 1
-    ./fio_single_test.sh aws_multi_luns $n read 0 256K $iodepth 180 1 
+    ./fio_single_test.sh aws_multi_luns $n write 0 64K $iodepth $times 1
+    ./fio_single_test.sh aws_multi_luns $n read 0 64K $iodepth $times 1
+    ./fio_single_test.sh aws_multi_luns $n write 0 128K $iodepth $times 1
+    ./fio_single_test.sh aws_multi_luns $n read 0 128K $iodepth $times 1
+    ./fio_single_test.sh aws_multi_luns $n write 0 256K $iodepth $times 1
+    ./fio_single_test.sh aws_multi_luns $n read 0 256K $iodepth $times 1
     let n++ 
     let iodepth++
 done
