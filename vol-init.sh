927cad9c7c0a6a56a4071af3ebd32e3b2ebd7eee
Leo <leoshch@gmail.com>
2018-12-13 18:13:22 +0800

    Create vol-init.sh

---
+++ vol-init.sh
@@ -0,0 +1,6 @@
+sudo su
+yum install -y fio git
+fio --filename=/dev/nvme1n1 --rw=write --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize | tee nvme1n1-init.txt &
+fio --filename=/dev/nvme2n1 --rw=write --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize | tee nvme2n1-init.txt &
+fio --filename=/dev/nvme3n1 --rw=write --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize | tee nvme2n1-init.txt &
+fio --filename=/dev/nvme4n1 --rw=write --bs=128k --iodepth=32 --ioengine=libaio --direct=1 --name=volume-initialize | tee nvme2n1-init.txt &
