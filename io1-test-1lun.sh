43894488a7eff59b20e0b716f75ede99e1fa1805
Leo <leoshch@gmail.com>
2018-12-14 05:24:03 +0000

    Update io1-test-1lun.sh

--- io1-test-1lun.sh
+++ io1-test-1lun.sh
@@ -5,16 +5,18 @@ iodepth=1
 #jobs=1
 times=180
 
+./fio_single_test.sh aws_single_lun $n randwrite 0 4K $iodepth $times 1
+./fio_single_test.sh aws_single_lun $n randread 0 4K $iodepth $times 1
+./fio_single_test.sh aws_single_lun $n randwrite 0 16K $iodepth $times 1
+./fio_single_test.sh aws_single_lun $n randread 0 16K $iodepth $times 1 
+
+iodepth=2
 while [ $iodepth -le 12 ]
 do
     ./fio_single_test.sh aws_single_lun $n randwrite 0 4K $iodepth $times 1
     ./fio_single_test.sh aws_single_lun $n randread 0 4K $iodepth $times 1
-    ./fio_single_test.sh aws_single_lun $n randwrite 0 8K $iodepth $times 1
-    ./fio_single_test.sh aws_single_lun $n randread 0 8K $iodepth $times 1
     ./fio_single_test.sh aws_single_lun $n randwrite 0 16K $iodepth $times 1
     ./fio_single_test.sh aws_single_lun $n randread 0 16K $iodepth $times 1 
-    ./fio_single_test.sh aws_single_lun $n randwrite 0 32K $iodepth $times 1
-    ./fio_single_test.sh aws_single_lun $n randread 0 32K $iodepth $times 1
     let n++ 
-    let iodepth++
+    let iodepth=$(( $iodepth + 2 ))
 done
